3add707de7b6a684304902c7c4e67ac12a620d7f
gizmore <gizmore@wechall.net>
2026-06-23 18:12:35 +0200

    sd

--- js/pygdo-choices.js
+++ js/pygdo-choices.js
@@ -1,6 +1,6 @@
 "use strict";
 window.gdo.choices = {
     gdo_init: function() {
-        selects = document.querySelectorAll("[gdo-autocomplete=]");
+        // selects = document.querySelectorAll("[gdo-autocomplete=]");
     },
 };
